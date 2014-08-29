b1b2a8c36dd1fe8dd3cd3a91eba4390e327a950c
arth-co <himanshu.kalra@gmail.com>
2014-08-29 14:03:05 +0530

    Test Change

--- eduapp/models.py
+++ eduapp/models.py
@@ -4,6 +4,7 @@ from django.utils.encoding import smart_text
 # Create your models here.
 # Change from HP.
 # Change by Himanshu
+# Change by Shubham
 
 class Questions(models.Model):
     question_id = models.CharField(max_length=10, null=False, blank=False)
